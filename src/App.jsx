0d24a09afee0ccc5366cb979865207379d153f02
Vennisha Naidoo <vennisha.naidoo@appstage.co.za>
2024-11-07 12:12:02 +0200

    Displaying Projects onto Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ function App() {
 
       return { 
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject]
       }
     });
@@ -48,7 +49,7 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={ handleStartAddProject } />
+      <ProjectsSidebar onStartAddProject={ handleStartAddProject } projects={ projectsState.projects } />
       { content }
     </main>
   );
