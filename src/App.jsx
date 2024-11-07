846b6707b6c3e2d6da583bc6fae7c6f24e242aeb
Vennisha Naidoo <vennisha.naidoo@appstage.co.za>
2024-11-07 15:52:41 +0200

    Delete Single Project

--- src/App.jsx
+++ src/App.jsx
@@ -56,6 +56,16 @@ function App() {
     });
   }
 
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId)
+      }
+    })
+  }
+
   const selectedProject =  projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
 
   let content;
@@ -63,7 +73,7 @@ function App() {
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={ handleAddProject } onCancel={ handleCancelAddPorject } />
   } else if (selectedProject) {
-    content = <Project project={ selectedProject } />;
+    content = <Project project={ selectedProject } onDelete={ handleDeleteProject } />;
   } else {
     content = <NoProjectSelected onStartAddProject={ handleStartAddProject } />
   }
